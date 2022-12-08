9b52b49cbeb8697e05ee5270ca8e0cfbe0581daf
FrankQiu <iam.frankqiu@gmail.com>
2022-12-08 16:52:01 +0800

    chore: remove the comments

--- src/utils.ts
+++ src/utils.ts
@@ -3,15 +3,6 @@ import { join } from 'path';
 
 export async function readPackageInfo() {
   const files = await readdir(join(__dirname, './../packages'));
-  //   const results = await Promise.all(
-  //     files.map(async (file) => {
-  //       const filePath = path.join('packages', file);
-  //       const stats = await fs.promises.stat(filePath);
-  //       return {
-  //         name: file,
-  //       };
-  //     }),
-  //   );
   return Array.from(
     new Set(files.map((item: string) => item.split('.').shift())),
   );
